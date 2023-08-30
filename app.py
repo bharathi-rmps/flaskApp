43d08dd1bb012449094f00c379d5a63e4788acf3
bharathi-rmps <79350260+bharathi-rmps@users.noreply.github.com>
2023-08-30 19:26:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -68,5 +68,5 @@ def meme_analysis():
     return analyze_meme()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=8000, debug=False)
     
